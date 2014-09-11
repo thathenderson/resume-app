e5d3d429cdc94fa946add361701e60355383c877
Michael Henderson <michaelbradh@gmail.com>
2014-09-11 17:52:03 -0400

    V2 Resume

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 //resume v01
 (function() {
+	//App config
 	var ResumeApp = angular.module('ResumeApp', [
 		'ngRoute',
 		'controllers'
 	]);
 
+	//Router
 	ResumeApp.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.
 			when('/experience', {
@@ -23,15 +25,20 @@
 				templateUrl: 'partials/_education.html',
 				controller: 'EducationController'
 			}).
+			when('/print', {
+				templateUrl: 'partials/_print.html',
+				controller: 'PrintController'
+			}).
 			otherwise({
 				redirectTo: '/experience'
 			});
 	}]);
 
+	//Factory to grab appropriate json
 	ResumeApp.factory('ResumeData', function($http) {
 		return { 
-			getResumeData: function(callback) {
-				$http.get('js/resume.json').success(callback);
+			getResumeData: function(type, callback) {
+				$http.get('js/resume_'+type+'.json').success(callback);
 			}
 		};
 	});
--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,48 +1,66 @@
 var controllers = angular.module('controllers', []);
 
 controllers.controller('MeController', function($scope, $location, ResumeData) {
-	ResumeData.getResumeData(function(data) {
+	ResumeData.getResumeData('me', function(data) {
 		$scope.me = data.me;
 	});
 
+	//check to see if any nav options are active
 	$scope.isActive = function(path) {
 		if ($location.path().substr(0, path.length) == path) {	
 			return true;
-			console.log('returned active class');
 		} else {
 			return false;
 		}
 	}
 
+	//open up the nav menu
 	$scope.showMenu = function() {
-		$('.nav').toggleClass('show');
+		$('.menu').toggleClass('active-menu-button');
+		$('.nav-options').toggleClass('show-menu');
 	}
 
+	//super, super hacky way to trigger print only after view has finished loading. I hate myself
+	$scope.printMe = function() {
+		window.setTimeout(window.print, 500);
+	}
+
+	//remove all menu class once something is selected
 	$scope.$on('$routeChangeStart', function() {
-		$('.nav').removeClass('show');
+		$('.menu').removeClass('active-menu-button');
+		$('.nav-options').removeClass('show-menu');
 	});
 });
 
 controllers.controller('ExperienceController', function($scope, ResumeData) {
-	ResumeData.getResumeData(function(data) {
+	ResumeData.getResumeData('jobs', function(data) {
 		$scope.jobs = data.jobs;
 	});
 });
 
 controllers.controller('AccomplishmentsController', function($scope, ResumeData) {
-	ResumeData.getResumeData(function(data) {
+	ResumeData.getResumeData('accm', function(data) {
 		$scope.accomplishments = data.accomplishments;
 	});
 });
 
 controllers.controller('SkillsController', function($scope, ResumeData) {
-	ResumeData.getResumeData(function(data) {
+	ResumeData.getResumeData('skills', function(data) {
 		$scope.skills = data.skills;
 	});
 });
 
 controllers.controller('EducationController', function($scope, ResumeData) {
-	ResumeData.getResumeData(function(data) {
+	ResumeData.getResumeData('edu', function(data) {
+		$scope.education = data.education;
+	});
+});
+
+controllers.controller('PrintController', function($scope, ResumeData) {
+	ResumeData.getResumeData('full', function(data) {
+		$scope.jobs = data.jobs;
+		$scope.accomplishments = data.accomplishments;
+		$scope.skills = data.skills;
 		$scope.education = data.education;
 	});
 });
